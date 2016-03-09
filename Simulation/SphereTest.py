9a4bb8a2e981310f60da472b11cdea75450cb005
Markus Gumbel <m.gumbel@hs-mannheim.de>
2016-03-09 09:16:34 +0100

    New main programs for testing.

---
+++ Simulation/SphereTest.py
@@ -0,0 +1,18 @@
+# Example for Simulation.
+
+# Important to have it here. Otherwise error. CC3D uses a special module loader
+# that cannot directly instantiate classes. (Wish I knew more on Python)
+import sys
+from os import environ
+
+import CompuCellSetup
+
+sys.path.append(environ["PYTHON_MODULE_PATH"])
+sim, simthread = CompuCellSetup.getCoreSimulationObjects()
+
+# Now load the model to simulate!
+import Settings.DirectoryPath
+from Test.Shape.SphereTest import SphereTest
+model = SphereTest(sim, simthread, Settings.DirectoryPath.getSrcPath())
+# srcDir is required to know where CellsInit.piff is.
+
---
+++ Simulation/VerifyGrowth.py
@@ -0,0 +1,21 @@
+# Example for Simulation.
+
+# Important to have it here. Otherwise error. CC3D uses a special module loader
+# that cannot directly instantiate classes. (Wish I knew more on Python)
+import sys
+from os import environ
+
+import CompuCellSetup
+
+sys.path.append(environ["PYTHON_MODULE_PATH"])
+sim, simthread = CompuCellSetup.getCoreSimulationObjects()
+
+# Now load the model to simulate!
+import Settings.DirectoryPath
+#model = VerifyGrowthT500D1(sim, simthread, Settings.DirectoryPath.getSrcPath())
+from Test.Scalability.VerifyGrowthT1000D1 import VerifyGrowthT1000D1
+model = VerifyGrowthT1000D1(sim, simthread, Settings.DirectoryPath.getSrcPath())
+#model = VerifyGrowthT2000D1(sim, simthread, Settings.DirectoryPath.getSrcPath())
+#model = VerifyGrowthT1000D2(sim, simthread, Settings.DirectoryPath.getSrcPath())
+# srcDir is required to know where CellsInit.piff is.
+
