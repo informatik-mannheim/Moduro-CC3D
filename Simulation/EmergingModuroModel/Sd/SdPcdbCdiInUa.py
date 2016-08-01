406f4c73b50933a323a1a7f3198709e65e224d94
Julian Debatin <juliandebatin@gmail.com>
2016-08-01 09:27:57 +0200

    no message

--- Simulation/EmergingModuroModel/Sd/SdPcdbCdiInUa.py
+++ Simulation/EmergingModuroModel/Sd/SdPcdbCdiInUa.py
@@ -49,22 +49,22 @@ class SdPcdbCdiInUa(ModelConfig):
         cellTypes = []
 
         stem = Stemcell
-        stem.setGrowthVolumePerDayRelVolume(0.05)
+        stem.setGrowthVolumePerDayRelVolume(0.03)
         self.stemNecrosisProb = stem.necrosisProb = 0
 
         basal = Basalcell
-        basal.setGrowthVolumePerDayRelVolume(0.05)
+        basal.setGrowthVolumePerDayRelVolume(0.03)
         basal.apoptosisTimeInDays = 800000.0
         self.basalNecrosisProb = basal.necrosisProb = 0.03
 
         intermediate = Intermediatecell
-        intermediate.setGrowthVolumePerDayRelVolume(0.05)
+        intermediate.setGrowthVolumePerDayRelVolume(0.02)
         intermediate.apoptosisTimeInDays = 20000000.0
         self.intermediateNecrosisProb = intermediate.necrosisProb = 0.04
 
 
         umbrella = Umbrellacell
-        umbrella.setGrowthVolumePerDayRelVolume(0.05)
+        umbrella.setGrowthVolumePerDayRelVolume(0.02)
         umbrella.apoptosisTimeInDays = 100000000.0
         self.umbrellaNecrosisProb = umbrella.necrosisProb = 0.06
 
