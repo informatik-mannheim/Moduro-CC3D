0931a8d69aa5618b7e8e195fcf9c2458801bdf56
Julian Debatin <juliandebatin@gmail.com>
2016-09-27 13:29:22 +0200

    no message

--- Simulation/EmergingModuroModel/Spa/SpaBpaPcdiInUa.py
+++ Simulation/EmergingModuroModel/Spa/SpaBpaPcdiInUa.py
@@ -59,12 +59,12 @@ class SpaBpaPcdiInUa(ModelConfig):
         intermediate = Intermediatecell
         intermediate.setGrowthVolumePerDayRelVolume(0.09)
         intermediate.apoptosisTimeInDays = 200000000.0
-        self.intermediateNecrosisProb = intermediate.necrosisProb = 0.00007
+        self.intermediateNecrosisProb = intermediate.necrosisProb = 0.00008
 
         umbrella = Umbrellacell
         umbrella.setGrowthVolumePerDayRelVolume(0.08)
         umbrella.apoptosisTimeInDays = 100000000.0
-        self.umbrellaNecrosisProb = umbrella.necrosisProb = 0.00008
+        self.umbrellaNecrosisProb = umbrella.necrosisProb = 0.00009
 
         stem.setDescendants(0.90, [stem.id, basal.id])
         stem.setDescendants(0.05, [stem.id, stem.id])
