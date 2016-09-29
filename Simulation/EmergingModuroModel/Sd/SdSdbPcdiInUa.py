c36e8f5813138c0094b04b82768a40d3bfb83b08
Julian Debatin <juliandebatin@gmail.com>
2016-09-29 09:33:09 +0200

    no message

--- Simulation/EmergingModuroModel/Sd/SdSdbPcdiInUa.py
+++ Simulation/EmergingModuroModel/Sd/SdSdbPcdiInUa.py
@@ -50,7 +50,7 @@ class SdSdbPcdiInUa(ModelConfig):
         cellTypes = []
         stem = Stemcell
         stem.setGrowthVolumePerDayRelVolume(0.12)
-        self.necrosisProbStem = stem.necrosisProb = 0.0
+        self.stemNecrosisProb = stem.necrosisProb = 0.0
 
         basal = Basalcell
         basal.setGrowthVolumePerDayRelVolume(0.11)
